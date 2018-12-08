f1b22bac1f7afd84b6871b45cf40b9a011ba508e
DQY01010 <1076269100@qq.com>
2018-12-08 14:28:56 +0800

    third

--- GratuationInterview/src/login.java
+++ GratuationInterview/src/login.java
@@ -19,27 +19,8 @@ public class login {
         public static Connection con;
         public static Statement stmt;
         public static ResultSet rs;
-    public static void main(String[] args) {    
-        // 创建 JFrame 实例
-        JFrame frame = new JFrame("登录");
-        // Setting the width and height of frame
-        frame.setSize(450, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static void main(String[] args) {
 
-        /* 创建面板，这个类似于 HTML 的 div 标签
-         * 我们可以创建多个面板并在 JFrame 中指定位置
-         * 面板中我们可以添加文本字段，按钮及其他组件。
-         */
-        JPanel panel = new JPanel();    
-        // 添加面板
-        frame.add(panel);
-        /* 
-         * 调用用户定义的方法并添加组件到面板
-         */
-        placeComponents(panel);
-
-        // 设置界面可见
-        frame.setVisible(true);
 
         try {
             System.out.println("登录：");
@@ -59,7 +40,26 @@ public class login {
         }
     }
 
-    private static void placeComponents(JPanel panel) {
+    public login() {
+// 创建 JFrame 实例
+        JFrame frame = new JFrame("登录");
+        // Setting the width and height of frame
+        frame.setSize(450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* 创建面板，这个类似于 HTML 的 div 标签
+         * 我们可以创建多个面板并在 JFrame 中指定位置
+         * 面板中我们可以添加文本字段，按钮及其他组件。
+         */
+        JPanel panel = new JPanel();
+        // 添加面板
+        frame.add(panel);
+        /*
+         * 调用用户定义的方法并添加组件到面板
+         */
+
+        // 设置界面可见
+        frame.setVisible(true);
 
         panel.setLayout(null);
 
